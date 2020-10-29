bdf57f426ff85b8900dda408bacb0d94e9f753b4
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-29 16:24:51 -0400

    Tests for area

--- PointTester.java
+++ PointTester.java
@@ -2,10 +2,11 @@ public class PointTester{
   public static void main(String[]args){
     Point One = new Point(2.0,4.0);
     Point Two = new Point(2.0,2.0);
-    Point Three = new Point(5.0, 5.0);
+    Point Three = new Point(5.0, 4.0);
     //System.out.println(One.distanceTo(Two));
     //System.out.println(One.equals(Two));
     Triangle First = new Triangle(One, Two, Three);
     System.out.println(First.getPerimeter());
+    System.out.println(First.getArea());
   }
 }
