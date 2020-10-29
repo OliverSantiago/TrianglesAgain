ba1eccb5a91d3b382f013591d8a61569a503336b
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-29 16:44:30 -0400

    Final Method

--- PointTester.java
+++ PointTester.java
@@ -2,13 +2,16 @@ public class PointTester{
   public static void main(String[]args){
     Point One = new Point(2.0,4.0);
     Point Two = new Point(2.0,2.0);
-    Point Three = new Point(5.0, 4.0);
+    Point Three = new Point(5.0,4.0);
     //System.out.println(One.distanceTo(Two));
     //System.out.println(One.equals(Two));
     Triangle First = new Triangle(One, Two, Three);
     System.out.println(First.getPerimeter());
     System.out.println(First.getArea());
     System.out.println(First.classify());
+    System.out.println(First.toString());
+    Point Four = new Point(6.0,4.0);
+    First.setVertex(3, Four);
     System.out.println(First.toString());
   }
 }
