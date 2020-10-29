b13e458ff1d38845ec5f155ec8c0d15e91fa4521
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-29 16:19:24 -0400

    Test cases for Triangle

--- PointTester.java
+++ PointTester.java
@@ -1,8 +1,11 @@
 public class PointTester{
   public static void main(String[]args){
-    Point One = new Point(2.0,2.0);
+    Point One = new Point(2.0,4.0);
     Point Two = new Point(2.0,2.0);
-    System.out.println(One.distanceTo(Two));
-    System.out.println(One.equals(Two));
+    Point Three = new Point(5.0, 5.0);
+    //System.out.println(One.distanceTo(Two));
+    //System.out.println(One.equals(Two));
+    Triangle First = new Triangle(One, Two, Three);
+    System.out.println(First.getPerimeter());
   }
 }
