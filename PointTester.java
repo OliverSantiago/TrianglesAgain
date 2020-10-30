df6761a0255a5da24d6dff2d43c9c1df270b4fe3
Oliver Santiago Kermani <osantiagokermani20@stuy.edu>
2020-10-29 23:08:44 -0400

    Another test

--- PointTester.java
+++ PointTester.java
@@ -3,12 +3,17 @@ public class PointTester{
     Point One = new Point(2.0,4.0);
     Point Two = new Point(2.0,2.0);
     Point Three = new Point(5.0,4.0);
+    Point A = new Point(0.0,0.0);
+    Point B = new Point(1,Math.sqrt(3));
+    Point C = new Point(2,0);
     //System.out.println(One.distanceTo(Two));
     //System.out.println(One.equals(Two));
     Triangle First = new Triangle(One, Two, Three);
+    Triangle Second = new Triangle(A, B, C);
     System.out.println(First.getPerimeter());
     System.out.println(First.getArea());
     System.out.println(First.classify());
+    System.out.println(Second.classify());
     System.out.println(First.toString());
     Point Four = new Point(6.0,4.0);
     First.setVertex(3, Four);
